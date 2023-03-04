5bf225706b90182826d1e02794dd0d8010a4f873
davicompu <villacis@vt.edu>
2023-03-03 21:49:16 -0700

    Finalizing code template and readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# CIS3301-CODE-11-Bubble-And-Insert-Sort-Algorithms
+## CIS3301-CODE-11-Bubble-And-Insert-Sort-Algorithms
+
+## Instructions
+
+In this CODE assignment you are asked to implement a bubble and a insert sort algorithm in Python. 
+
++ Bubble Sort Algorithm
+  +  Implement your code in `get_bubble_sorted_list`
++ Insert Sort Algoritm
+  +  Implement your code in `get_insert_sorted_list`
+
+To test your code, call your functions inside the `if __name__ == "__main__":` statement. You can also use the function `get_random_list_of_items` to create list of numerical items to test your code.
+
+
--- code_11.py
+++ code_11.py
@@ -1,8 +1,17 @@
+import random
 def get_bubble_sorted_list(list):
     pass #Remove this line and implement a bubble sort algorithm
 
 def get_insert_sorted_list(list):
     pass #Remove this line and implement a bubble insert algorithm
+
+#You can use the function below to test your sorting algorithm implementation.
+def get_random_list_of_items():
+    list_length = random.randint(10,20)
+    new_list = []
+    for i in range(1,list_length):
+        new_list.append(random.randint(100,500))
+    return new_list
 
 if __name__ == "__main__":
     pass #Remove this line and program your user interface to test your functions
